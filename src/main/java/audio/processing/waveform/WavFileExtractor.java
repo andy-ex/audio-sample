50945abfd2c6669766c3041dc3c3dd763928f0b2
Dmitry <volchek.mail@gmail.com>
2015-01-04 19:02:16 +0300

    Add new Wav File extractor

---
+++ src/main/java/audio/processing/waveform/WavFileExtractor.java
@@ -0,0 +1,22 @@
+package audio.processing.waveform;
+
+import java.io.File;
+
+public class WavFileExtractor implements WaveformExtractor {
+    @Override
+    public double[] extract(File in) {
+        try {
+            WavFile wavFile = WavFile.openWavFile(in);
+
+            double[] result = new double[Integer.valueOf(String.valueOf(wavFile.getNumFrames()))];
+            wavFile.readFrames(result, result.length);
+
+            wavFile.close();
+
+            return result;
+        } catch (Exception e) {
+            System.out.println("Error reading audio file: " + e.getMessage());
+            return new double[0];
+        }
+    }
+}
