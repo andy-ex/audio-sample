2a3ae8a9457576fbdba43e7d59a0281e027fc148
Dmitry <volchek.mail@gmail.com>
2015-05-03 21:42:23 +0300

    Save to arff

--- src/main/java/sample/CrossValidation.java
+++ src/main/java/sample/CrossValidation.java
@@ -1,7 +1,10 @@
 package sample;
 
 import audio.classification.Label;
+import audio.classification.MachineLearningClassifier;
 import audio.classification.knn.KNearestNeighbors;
+import audio.classification.svm.MultiClassSupportVectorMachine;
+import audio.classification.svm.SupportVectorMachine;
 import audio.domain.*;
 import audio.features.cache.FeaturesCache;
 
@@ -13,13 +16,25 @@ public class CrossValidation {
     private static final int SAMPLES_TO_TRAIN = 89;
     private static final int SAMPLES_TO_VALIDATE = 10;
     private static final String feature = FeatureName.MFCC.getName();
+    private static List<String> genres = Arrays.asList("rock", "classical", "jazz", "hiphop");;
 
     public static void main(String[] args) throws URISyntaxException {
         test();
     }
 
+    private static void saveSoundArff() {
+        Sounds sounds = FeaturesCache.loadSounds();
+
+        System.out.println("@DATA");
+        for (Genre genre : sounds.getGenres()) {
+            if (!genres.contains(genre.getGenreId())) continue;
+            for (Sound sound : genre.getSounds()) {
+                System.out.println(sound.getFeatures().getFeatureValue(feature) + "," + genre.getGenreId());
+            }
+        }
+    }
+
     public static void test() throws URISyntaxException {
-        List<String> genres = Arrays.asList("metal", "classical");
         Map<Label, Set<double[]>> trainData = new HashMap<>();
         Sounds sounds = FeaturesCache.loadSounds();
         int genreId = 0;
@@ -36,8 +51,8 @@ public class CrossValidation {
             trainData.put(new Label(genreId++, genre.getGenreId()), featureData);
         }
 
-        KNearestNeighbors knn = new KNearestNeighbors();
-        knn.train(trainData);
+        MachineLearningClassifier classifier = new MultiClassSupportVectorMachine();
+        classifier.train(trainData);
 
         int correct = 0;
         for (Genre genre : sounds.getGenres()) {
@@ -46,7 +61,7 @@ public class CrossValidation {
             List<Sound> genreSounds = genre.getSounds();
             for (int i = SAMPLES_TO_TRAIN; i < SAMPLES_TO_TRAIN + SAMPLES_TO_VALIDATE; i++) {
                 Sound sound = genreSounds.get(i);
-                Label predicted = knn.predict(new double[]{sound.getFeatures().getFeatureValue(feature)});
+                Label predicted = classifier.predict(new double[]{sound.getFeatures().getFeatureValue(feature)});
                 System.out.println(sound.getPath().substring(sound.getPath().lastIndexOf("\\")) + " : " + predicted.getName());
 
                 if (predicted.getName().equals(genre.getGenreId())) correct++;
